015ac4d92d08e5cdc4a1a89dfb8e136fc38ef7d0
rb2001 <rbaird2001@yahoo.com>
2019-07-17 11:05:25 -0600

    game logic code working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,83 @@
+/*(function () {
+    // do your setup
+
+})();*/
+
 var objGame = {
     //function to load wordlist needed
+    //list of words to play
     arrWordList : ["bandersnatch", "giggledick"],
-    strWordChoice : function () {
-        this.arrWordList[Math.floor(Math.random() * arrWordList.length)];
+    //use a random function to select a word 
+    get strWordChoice(){
+        return this.arrWordList[Math.floor(Math.random() * this.arrWordList.length)];
     },
-    arrKeysToMatch : {},
-    addKeysToMatch : function (choice = this.strWordChoice) {
+    //object of letters in selected work and their positions
+    // supports multiple instances of same letter in a word
+    objKeysToMatch : {},
+    //function that populates the objKeysToMach object
+    addKeysToMatch : function () {
+        choice = this.strWordChoice;
         for (i = 0; i < choice.length; i++) {
             var strLetter = choice[i];
-            if (objGame.arrKeysToMatch[strLetter]) {
-                objGame.arrKeysToMatch[strLetter].push(i);
+            if (this.objKeysToMatch[strLetter]) {
+                this.objKeysToMatch[strLetter].push(i);
             }
             else {
-            objGame.arrKeysToMatch[strLetter] = [i];
+            this.objKeysToMatch[strLetter] = [i];
             }
         }
     },
+    //list of keys played
     arrKeysPlayed : [],
-    strSelectedKey : event.key,
+    //This will need to pickup the value of the event.key
+    strSelectedKey : "",
     //boolKeyValidator needs code to examine key against valid keys. Look to regex for assitance
     boolKeyValidator: function () { },
     intMatches : 0,
-    intMatchWin : this.strWordChoice ,
-    playTheKey : function(key = this.strSelectedKey){
-
+    // TODO:change this to account for duplicate keys in word using arrKeysToMatch.length
+    get intMatchWin() {
+        return Object.keys(this.arrKeysToMatch).length;
+    },
+    //list of keys played that do not match the word.
+    arrNegMatches : [],
+    //number of missed selections before game ends with loss
+    intMatchLose : 6,
+    //function that plays each selected letter against the chosen word.
+    playTheKey : function(){
+        if(this.arrKeysPlayed.indexOf(this.strSelectedKey)){
+                //TODO:code for actions if selected key already played.
+                alert("Letter already played. Play another letter.");
+                return;
+            }
+        else {
+            this.arrKeysPlayed.push(this.strSelectedKey);
+            if (this.arrKeysToMatch[this.strSelectedKey]) {
+                //create intMatches
+                this.intMatches++
+                //TODO: add code to position keys onto playboard
+        
+                if (this.intMatches === this.intMatchWin) {
+                    //TODO: code to indicate win to player and end game
+                }
+            }
+            else {
+                //add selected key to list of missed matches
+                this.arrNegMatches.push(this.strSelectedKey)
+                //code displaying updated unmatched keys needed
+                if (this.arrNegMatches.length === this.intMatchLose) {
+                    //code to indicate loss and end game.
+                }
+            }        
+        }
+    },
+    setPlaceholders :  function(){
+        for (i=0; i<Object.keys(this.objKeysToMatch).length;i++){
+            var newLi = document.createElement("li");
+            var placeholder = document.createTextNode("__");
+            newLi.appendChild(placeholder);
+            newLi.setAttribute("class", "letterPlacer");
+            newLi.setAttribute("id","li" + i);
+            document.getElementById("wordPlacer").appendChild(newLi);
+        }
     },
 }
-
-
-
-
-
-
-
-
-
-//need validator to verify proper key selected (a-z).
-//check to see if key played    
-if(arrKeysPlayed.indexOf(strSelectedKey)){
-        //code for actions if selected key already played.
-    }
-else {
-    arrKeysPlayed.push(strSelectedKey);
-    if (objGame[strSelectedKey]) {
-        //create intMatches
-        intMatches = intMatches++
-        //add code to position keys onto playboard
-
-        if (intMatches === intMatchWin) {
-            //code to indicate win to player and end game
-        }
-
-    }
-    else {
-        //array holding unmatched keys needed
-        arrNegMatches.push(strSelectedKey)
-        //code displaying updated unmatched keys needed
-        //arrNegMatches needs creation
-        if (arrNegMatches.length === intMatchLose) {
-            //code to indicate loss and end game.
-        }
-    }
-
-}
-
-
-
-// function makeDashes() {
-
-//     var strDashes = "-"
-//     for(i=0;i<strWordChoice.length;i++){
-//     strDashes += " -";
-
-
-//     }
-//     console.log(strDashes);
-// }
-
-
-
-
-
